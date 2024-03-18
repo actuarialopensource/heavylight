2b36c32cb6ddf8e88333b5f6c435462d1193ba5d
Matthew Caseres <matthewcaseres@outlook.com>
2024-03-18 12:41:44 +0000

    setup tests

--- src/heavylight/heavylight.py
+++ src/heavylight/heavylight.py
@@ -3,6 +3,7 @@ import types
 from inspect import signature, getmembers
 import pandas as pd
 
+
 class Table:
     """A Table has one or more keys, and a single column of values"""
 
@@ -12,17 +13,17 @@ class Table:
 
     def __getitem__(self, key):
         return self.series.at[key]
-    
+
     def __call__(self, key):
         return self.series.at[key]
-    
+
     def values(self, key):
         return self.series.loc[key].values
 
     @staticmethod
-    def read_csv(filename, sep=",", type_identifier = "|"):
+    def read_csv(filename, sep=",", type_identifier="|"):
         """Read in a table from a csv file, type encoding is via the header:
-        
+
         <keyname1>|<type1>,<keyname2>|<type2>....<value>|<typeN>
         where type is one of "str", "int", "float"
         """
@@ -30,8 +31,10 @@ class Table:
         with open(filename, "r") as csv_file:
             header = next(csv_file).strip("\n").split(sep)
         tid = type_identifier
-        header_mapper_str = {item:item.split(tid)[1] for item in header}
-        header_mapper_types = {col:column_types[val] for col, val in header_mapper_str.items()}
+        header_mapper_str = {item: item.split(tid)[1] for item in header}
+        header_mapper_types = {
+            col: column_types[val] for col, val in header_mapper_str.items()
+        }
         df = pd.read_csv(filename, sep=sep, dtype=header_mapper_types)
         # strip `tid` from column names
         df.columns = [col.split("|")[0] for col in df.columns]
@@ -60,18 +63,18 @@ class _Cache:
 
     def __repr__(self):
         return f"<Cache Function: {self.func.__name__} Size: {len(self._store)}>"
-    
+
     def sum(self):
         """return the sum of all values in the Cache Function"""
         return sum(self._store.values())
-    
+
     @property
     def values(self):
         return list(self._store.values())
 
 
 class Model:
-    def __init__(self, *, do_run = False, verbose = False, proj_len = None, **kwargs,):
+    def __init__(self, *, do_run=False, verbose=False, proj_len=None, **kwargs):
         """Base Class to subclass for user models.
 
         All variables/methods in user models should be lower case, using underscore as spaces.
@@ -99,7 +102,7 @@ class Model:
             print("    do_run:", do_run)
             print("    proj_len:", proj_len)
             print()
-        
+
         if do_run:
             if not isinstance(proj_len, int):
                 raise ValueError("proj_len must be an integer")
@@ -107,66 +110,82 @@ class Model:
                 raise ValueError("proj_len must have value greater than 0")
             self.proj_len = proj_len
         else:
-            if verbose: print("== Not Running - call Run() manually ==")
-       
-        if verbose: print("== Storing Arguments ==")
+            if verbose:
+                print("== Not Running - call Run() manually ==")
+
+        if verbose:
+            print("== Storing Arguments ==")
         for k, v in kwargs.items():
             if k in dir(self):
-                warnings.warn("Warning: Duplicate Item: "+str(k))
+                warnings.warn("Warning: Duplicate Item: " + str(k))
             setattr(self, k, v)
             if verbose:
                 print("    Updated: ", k, " : ", v)
 
         # cacheify
-        if verbose: print("== Caching Functions ==")
+        if verbose:
+            print("== Caching Functions ==")
         self._cache_funcs(verbose)
 
         if do_run and proj_len > 0:
             self.RunModel(proj_len, verbose)
-        if verbose: print("== Run complete == ")
+        if verbose:
+            print("== Run complete == ")
 
-
-    def RunModel(self, proj_len, verbose = False):
+    def RunModel(self, proj_len, verbose=False):
         if self._is_run:
             raise ValueError("Run has already been completed.")
 
-        if verbose: print(f"== Running Projection | length: {proj_len} ==")
-        
+        if verbose:
+            print(f"== Running Projection | length: {proj_len} ==")
+
         if hasattr(self, "BeforeRun"):
-            if verbose: print("    Calling BeforeRun")
+            if verbose:
+                print("    Calling BeforeRun")
             self.BeforeRun()
 
         if not self._cached:
-            raise ValueError("Functions have not been cached")  # NB: this shouldn't occur as now caching in instance
+            raise ValueError(
+                "Functions have not been cached"
+            )  # NB: this shouldn't occur as now caching in instance
         for t in range(proj_len):
             for name, func in self._funcs.items():
-                #func = getattr(self, var)
-                if func.has_one_param:   # skip functions with more than one parameter
-                    func(t)   #call each function in turn, starting from t==0
+                # func = getattr(self, var)
+                if func.has_one_param:  # skip functions with more than one parameter
+                    func(t)  # call each function in turn, starting from t==0
         self._is_run = True
         if hasattr(self, "AfterRun"):
-            if verbose: print("    Calling AfterRun")
+            if verbose:
+                print("    Calling AfterRun")
             return self.AfterRun()
-    
+
     def _cache_funcs(self, verbose: bool = False):
         if self._cached:
-            raise ValueError("Cache has already been set-up, please create a new instance")
+            raise ValueError(
+                "Cache has already been set-up, please create a new instance"
+            )
 
         self._funcs = {}
 
         for method_name, method in getmembers(self):
-            #method = getattr(self, method_name)
+            # method = getattr(self, method_name)
 
-            if method_name[0] != "_" and method_name[0].islower() and isinstance(method, types.MethodType):
-                param_count = len(signature(method).parameters) # count the parameters in the function.
+            if (
+                method_name[0] != "_"
+                and method_name[0].islower()
+                and isinstance(method, types.MethodType)
+            ):
+                param_count = len(
+                    signature(method).parameters
+                )  # count the parameters in the function.
                 cached_method = _Cache(method, param_count)
                 setattr(self, method_name, cached_method)
                 self._funcs[method_name] = cached_method
-                if verbose: print(f"    Cached: {method_name}")
+                if verbose:
+                    print(f"    Cached: {method_name}")
 
-        
         self._cached = True
-        
+
     def ToDataFrame(self):
         """return a pandas dataframe of all single parameter columns"""
         df = pd.DataFrame()
